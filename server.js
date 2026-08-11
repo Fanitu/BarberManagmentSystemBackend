dedd042a8fe192d700db8df7f3ea01075f60ecb5
Fanitu <FaniziG8@gmail.com>
2026-08-11 17:32:51 +0300

    Fix allowed origins format in server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(helmet());
 
 
 const ALLOWED_ORIGINS = (
-  process.env.ALLOWED_ORIGINS || "https://barber-super-admin-seven.vercel.app/,https://barber-managment-system.vercel.app/"
+  process.env.ALLOWED_ORIGINS || "https://barber-super-admin-seven.vercel.app,https://barber-managment-system.vercel.app"
 )
   .split(",")
   .map((o) => o.trim())
